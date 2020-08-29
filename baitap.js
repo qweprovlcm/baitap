5fdcc12d9a8dbc6a2acd58ac832e622d2d6a3bb6
Nguyen Minh Duc <trongdiemhl@gmail.com>
2020-08-29 20:03:47 +0700

    first commit

---
+++ baitap.js
@@ -0,0 +1,33 @@
+/*
+1.
+    1, a.var 1a2
+     b.var -studentCount
+     c.var var
+    2, By using keyword 'typeof' to check the type of variables.
+2. 1-b 2-a 3-e 4-c
+*/
+let message = 'Coding is great';
+console.log(message)
+let studentCount = 0;
+console.log(studentCount)
+message = 'Coding might not be easy, but still great';
+console.log(message)
+studentCount = 16;
+console.log(studentCount)
+message = 'coding might not be easy, but still great';
+console.log(message)
+studentCount = studentCount + 1;
+console.log(studentCount)
+alert('you look beautiful today');
+let hi = prompt('Hi there, what is your name?');
+alert('hi '+ hi)
+let firstName = prompt('Nhap ten ');
+let lastName = prompt('Nhap ho ');
+alert('ten la: ' + lastName + firstName)
+let canhHinhVuong = prompt('Nhap canh ');
+alert('dien tich: ' + canhHinhVuong*canhHinhVuong)
+let banKinh = prompt('Nhap ban kinh ');
+const pi = 3.14;
+alert('dien tich: ' + banKinh*pi)
+let celsius = prompt('Nhap do C ');
+alert(celsius + ' C = ' + (celsius * 1,8 + 32) +' F')
---
+++ index.js
@@ -0,0 +1,17 @@
+let number = 10;
+const myAge = "19";
+const myName = "Duc";
+
+console.log(myAge)
+console.log(myName)
+// alert('hello world')
+
+// const firstName = prompt('Your first name??');
+
+// console.log('Your first name ' + firstName)
+// alert('Your first name '+ firstName)
+
+const isPressOk = confirm('Press a button!');
+alert('confirm ==> ' + isPressOk)
+
+// == voi === khac nhau ntn
